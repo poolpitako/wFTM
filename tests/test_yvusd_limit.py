79b4c2c0b896e4980d5270dbb94a894991d68844
Poolpi Tako <poolpitako@protonmail.com>
2021-03-16 14:13:56 -0500

    fix: fix test

--- tests/test_yvusd_limit.py
+++ tests/test_yvusd_limit.py
@@ -16,23 +16,3 @@ def test_yvusd_limit(wFTM, wFTM_whale, vault, strategy, gov, fusdVault):
 
     assert fusdVault.availableDepositLimit() == 0
     assert strategy.balanceOfFusdInVault() > 0
-
-
-def test_yvusd_available_limit_is_zero(
-    wFTM, wFTM_whale, vault, strategy, gov, fusdVault
-):
-    amount = Wei("10000 ether")
-    wFTM.transfer(gov, amount, {"from": wFTM_whale})
-    wFTM.approve(vault, 2 ** 256 - 1, {"from": gov})
-
-    vault.deposit(amount, {"from": gov})
-
-    # Force the limit to 100 fUSD
-    fusd_gov = accounts.at(fusdVault.governance(), force=True)
-    fusdVault.setDepositLimit(Wei("100 ether"), {"from": fusd_gov})
-
-    # Invest
-    strategy.harvest({"from": gov})
-
-    assert fusdVault.availableDepositLimit() == 0
-    assert strategy.balanceOfFusdInVault() > 0
